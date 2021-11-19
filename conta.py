6fd5c84c8d3560e05cede4cc12f66814161af86a
Claudio <claudiosfcarvalho@outlook.com>
2021-11-18 21:43:32 -0300

    evoluindo classe conta e desafio com classe datas

--- conta.py
+++ conta.py
@@ -1,11 +1,20 @@
-
-
 class Conta:
 
-    #Construtor
+    # Construtor
     def __init__(self, numero, titular, saldo, limite):
         print(f"Contruindo objeto ...{self}")
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print(f"Saldo {self.saldo} do titular {self.titular}")
+
+    def deposita(self, valor):
+        print(f"Depósito de R${valor} na conta {self.numero}")
+        self.saldo += valor
+
+    def saca(self, valor):
+        print(f"Saque de R${valor} da conta {self.numero}")
+        self.saldo -= valor
