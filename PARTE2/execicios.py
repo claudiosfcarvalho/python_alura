acdba3b8afc8b3b37aac4386f22c2210d97dd83b
Claudio <claudiosfcarvalho@outlook.com>
2021-11-21 18:55:38 -0300

    colocando likes

--- PARTE2/execicios.py
+++ PARTE2/execicios.py
@@ -2,9 +2,14 @@ from modelo import Filme
 from modelo import Serie
 print("TESTE - Modelo - Inicio")
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
+vingadores.dar_like()
 print(f"Nome: {vingadores.nome} - Ano: {vingadores.ano} "
-      f"- Duração: {vingadores.duracao}")
+      f"- Duração: {vingadores.duracao} "
+      f"- Likes: {vingadores.likes}")
 
 atlanta = Serie("atlanta", 2018, 2)
+atlanta.dar_like()
+atlanta.dar_like()
 print(f"Nome: {atlanta.nome} - Ano: {atlanta.ano} "
-      f"- Temporadas: {atlanta.temporadas}")
+      f"- Temporadas: {atlanta.temporadas} "
+      f"- Likes: {atlanta.likes}")
