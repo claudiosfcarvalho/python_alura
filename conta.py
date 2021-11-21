565dd43242f7d84dfdddbd881589dbd4f3560abb
Claudio <claudiosfcarvalho@outlook.com>
2021-11-21 14:04:06 -0300

    alterando atributos para privado

--- conta.py
+++ conta.py
@@ -3,18 +3,23 @@ class Conta:
     # Construtor
     def __init__(self, numero, titular, saldo, limite):
         print(f"Contruindo objeto ...{self}")
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        # os dois underscore antes do campo indica campo privado
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f"Saldo {self.saldo} do titular {self.titular}")
+        print(f"Saldo {self.__saldo} do titular {self.__titular}")
 
     def deposita(self, valor):
-        print(f"Depósito de R${valor} na conta {self.numero}")
-        self.saldo += valor
+        print(f"Depósito de R${valor} na conta {self.__numero}")
+        self.__saldo += valor
 
     def saca(self, valor):
-        print(f"Saque de R${valor} da conta {self.numero}")
-        self.saldo -= valor
+        print(f"Saque de R${valor} da conta {self.__numero}")
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
