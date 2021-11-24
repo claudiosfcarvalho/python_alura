1b1ab3d8002d2d7cae0fed9c2bd34e1668722b3f
Claudio <claudiosfcarvalho@outlook.com>
2021-11-24 17:20:48 -0300

    trabalhando com parametros e len

--- PARTE 3 - Manipulacao de STRING/main.py
+++ PARTE 3 - Manipulacao de STRING/main.py
@@ -2,15 +2,15 @@ url = 'https://bytebank.com/cambio?moedaOrigem=real'
 
 print(url)
 
-# url_base = url[0:19]
-# print(url_base)
-#
-# url_parametros = url[20:36]
-# print(url_parametros)
-
 indice_interrogacao = url.find('?')
 url_base = url[:indice_interrogacao]
 print(url_base)
 
 url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
+
+parametro_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_parametros[indice_valor:]
+print(valor)
