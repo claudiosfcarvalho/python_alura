77d657510b5a6d4369a490012de8b297a279d965
Claudio <claudiosfcarvalho@outlook.com>
2021-11-24 17:11:15 -0300

    metodo find

--- PARTE 3 - Manipulacao de STRING/main.py
+++ PARTE 3 - Manipulacao de STRING/main.py
@@ -1,9 +1,16 @@
-url = 'bytebank.com/cambio?moedaOrigem=real'
+url = 'https://bytebank.com/cambio?moedaOrigem=real'
 
 print(url)
 
-url_base = url[0:19]
+# url_base = url[0:19]
+# print(url_base)
+#
+# url_parametros = url[20:36]
+# print(url_parametros)
+
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
 print(url_base)
 
-url_parametros = url[20:36]
+url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
