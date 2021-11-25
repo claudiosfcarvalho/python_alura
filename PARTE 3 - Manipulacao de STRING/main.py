7db6d354fb1f55e4ecae61d6f6d84e9c85f8ccd4
Claudio <claudiosfcarvalho@outlook.com>
2021-11-25 15:21:37 -0300

    fim do projeto de estudo

--- PARTE 3 - Manipulacao de STRING/main.py
+++ PARTE 3 - Manipulacao de STRING/main.py
@@ -1,16 +1,18 @@
 from extrator_url import ExtratorURL
 
 url = 'https://bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real  \t\n'
-
+VALOR_DOLAR = 5.50 #em real
 extrator_url = ExtratorURL(url)
 print(f'Tamanho URL:{len(extrator_url)}')
 print(extrator_url)
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
-print(f'quantidade: {valor_quantidade}')
+print(type(valor_quantidade))
+valor_convertido =  float(valor_quantidade) * VALOR_DOLAR
+print(f'Quantidade: R${valor_quantidade} \nConversão: ${valor_convertido}')
 
-extrator_url2 = ExtratorURL(url)
-print(extrator_url == extrator_url2)
-print(1 == True)
-print(1 is True)
-print(1 is 1)
-print(0 == False)
+# extrator_url2 = ExtratorURL(url)
+# print(extrator_url == extrator_url2)
+# print(1 == True)
+# print(1 is True)
+# print(1 is 1)
+# print(0 == False)
